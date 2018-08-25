ec5965cec31b46f1edf1edaf4a63893729327a5f
Saurabh Ghosh <kkooldandy@gmail.com>
2018-08-26 02:45:47 +0530

    Fixed server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/ng6Proj1'));
+app.use(express.static(__dirname + '/dist/ng6Proj1'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, './dist/ng6Proj1', 'index.html'));
+    res.sendFile(path.join(__dirname + '/dist/ng6Proj1/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () =>{
