05642e500d7ca0514d69853882ae65287e3c37bc
Saurabh Ghosh <kkooldandy@gmail.com>
2018-08-26 02:54:58 +0530

    Update server.js
    updated server.js

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/app/dist/ng6Proj1'));
+app.use(express.static('/dist/ng6Proj1'));
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/app/dist/ng6Proj1/index.html'));
+    res.sendFile(path.join('/dist/ng6Proj1/index.html'));
 });
 
 app.listen(process.env.PORT || 8080, () =>{
